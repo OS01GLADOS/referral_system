b74064f28ac664a4e8fa327e85ddd3f55a42163b
Taro <taro.yamado@gmail.com>
2025-07-24 19:56:33 +0300

    change db to postgres

--- referal_system/entrypoint.sh
+++ referal_system/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+python manage.py migrate
+
 export DJANGO_SUPERUSER_PASSWORD=test_root_password
 python manage.py createsuperuser --no-input --username taro
 python manage.py collectstatic --no-input
